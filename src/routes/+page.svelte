76746c9789b6768ee4e047fe2ba09210a841bb2d
Christophe PENAUD <christophe.penaud@gmail.com>
2025-02-28 15:35:46 +0100

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
 	import { client } from '$lib/graphqlClient';
 	import { gql } from 'graphql-tag';
 	import { onMount } from 'svelte';
   
-	let pros = [];
+	type Pro = {
+	  id: string;
+	  title: string;
+	};
+  
+	let pros: Pro[] = [];
   
 	const GET_PROS = gql`
 	  query {
@@ -11,31 +16,24 @@
 		  nodes {
 			id
 			title
-			allMeta {
-			  key
-			  value
-			}
 		  }
 		}
 	  }
 	`;
   
 	onMount(async () => {
-	  const result = await client.query(GET_PROS).toPromise();
+	  const result = await client.query(GET_PROS,{}).toPromise();
+	  console.log('Résultat GraphQL:', result);
 	  pros = result.data?.pros?.nodes || [];
 	});
   </script>
+  
   
   <h1>Liste des professionnels</h1>
   <ul>
 	{#each pros as pro}
 	  <li>
 		<strong>{pro.title}</strong>
-		<ul>
-		  {#each pro.allMeta as meta}
-			<li>{meta.key}: {meta.value}</li>
-		  {/each}
-		</ul>
 	  </li>
 	{/each}
   </ul>
