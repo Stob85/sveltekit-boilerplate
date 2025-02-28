9090d372e765ebfa2d0050c54a12d35ee57e4cae
Christophe PENAUD <christophe.penaud@gmail.com>
2025-02-28 15:15:42 +0100

    re

---
+++ src/lib/graphqlClient.js
@@ -0,0 +1,15 @@
+import { Client, cacheExchange, fetchExchange } from '@urql/svelte';
+
+export const client = new Client({
+    url: 'http://localhost:3000/graphql',
+    exchanges: [cacheExchange, fetchExchange],
+    fetchOptions: () => {
+      return {
+        headers: {
+            'Content-Type': 'application/json',
+          }
+      };
+    },
+  });
+  
+
